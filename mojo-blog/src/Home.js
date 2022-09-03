732ce6bede9774cbb45f1ea5fe213706aca8c640
Kira Corbett <kiracorbett00@gmail.com>
2022-09-03 07:59:57 -0700

    wip: click events

--- mojo-blog/src/Home.js
+++ mojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  
+  const handleClick = (e) => {
+    console.log('hello, world', e);
+  }
+
+  const clickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+  
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => clickAgain('mojo')}>Click again</button>
     </div>
    );
 }
