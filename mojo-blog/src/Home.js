15566c89acc868d75c8ca76c735ce2139ed32bc4
Kira Corbett <kiracorbett00@gmail.com>
2022-09-14 20:15:23 -0700

    displaying multiple reusable components

--- mojo-blog/src/Home.js
+++ mojo-blog/src/Home.js
@@ -5,13 +5,14 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'New site', body: 'lorem ipsum...', author: 'mario', id: 1},
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    { title: 'Mario kart', body: 'lorem ipsum...', author: 'toad', id: 3}
+    { title: 'Mario kart', body: 'lorem ipsum...', author: 'mario', id: 3}
   ]);
 
   // use props: (1) reusable component, (2) can be used in Home if needed, (3) practice with props
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/>
     </div>
    );
 }
