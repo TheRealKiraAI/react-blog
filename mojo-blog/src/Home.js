ce920f82dda8a4a8fc2797842b60c1dc44d911cd
Kira Corbett <kiracorbett00@gmail.com>
2022-09-14 20:29:02 -0700

    wip: exploring states

--- mojo-blog/src/Home.js
+++ mojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,12 @@ const Home = () => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  // useEffect re-renders code
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  });
 
   return ( 
     <div className="home">
