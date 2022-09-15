15842db965433a8686e4fd920e2b19404d37ea29
Kira Corbett <kiracorbett00@gmail.com>
2022-09-14 19:54:13 -0700

    outputting lists

--- mojo-blog/src/Home.js
+++ mojo-blog/src/Home.js
@@ -1,25 +1,20 @@
 import { useState } from 'react';
 
 const Home = () => {
-  // let name = 'mojo';
-  const [name, setName] = useState('mojo');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('jojo');
-    setAge(30);
-  }
-
-  const clickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'New site', body: 'lorem ipsum...', author: 'mario', id: 1},
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 1},
+    { title: 'Mario kart', body: 'lorem ipsum...', author: 'toad', id: 1}
+  ]);
   
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => clickAgain('mojo')}>Click again</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by: { blog.author }</p>
+        </div>
+      ))}
     </div>
    );
 }
